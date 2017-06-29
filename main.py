64144f84cc9a4ce770d12fb89e329e15583fc34a
thekingofcity <1634814279@qq.com>
2017-06-29 23:08:58 +0800

    Add virtualenv and check whether the output is correct

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
-'''
-    An easy fork to pytesseract
-    From https://github.com/madmaze/pytesseract/blob/95457153ca232eb854a6e80da17090f2fdb3c708/src/pytesseract.py
-    By thekingofcity 06/25/17
-'''
-
+import subprocess
 from gtts import gTTS
 from PIL import ImageGrab
-import subprocess
 
 
 def main():
-    snip()
-    status, error_string = tesseract("snip1.jpg", "out1")
-    if status:
-        print('Something wrong.')
-        return
-    status, error_string = tesseract("snip2.jpg", "out2")
-    if status:
-        print('Something wrong.')
-        return
-    # read("out1.txt")
-    # read("out2.txt")
-    tts("out1.txt", "out2.txt", "out.mp3")
+    # snip()
+    # status, error_string = tesseract("snip1.jpg", "out1")
+    # if status:
+    #     print('Something wrong.')
+    #     return
+    # status, error_string = tesseract("snip2.jpg", "out2")
+    # if status:
+    #     print('Something wrong.')
+    #     return
+    if tts("out1.txt", "out2.txt", "out.mp3") == 0:
+        print("Something wrong")
 
 
 def snip():
@@ -29,10 +22,22 @@ def snip():
     img.save("snip1.jpg", "JPEG", quality=75)
     img = ImageGrab.grab((1770, 115, 1850, 140))
     img.save("snip2.jpg", "JPEG", quality=75)
+    # img = ImageGrab.grab((23, 0, 133, 20))
+    # img.save("snip3.jpg", "JPEG", quality=90, dpi=(96,96))
 
 
-def tesseract(input_filename, output_filename):
-    command = ['C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe', input_filename, output_filename]
+def tesseract(input_filename, output_filename, lang="eng"):
+    '''
+    An easy fork to pytesseract
+    From https://github.com/madmaze/pytesseract/blob/95457153ca232eb854a6e80da17090f2fdb3c708/src/pytesseract.py
+    By thekingofcity 06/25/17
+    '''
+    command = [
+        'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe',
+        input_filename,
+        output_filename,
+        "-l " + lang
+        ]
     proc = subprocess.Popen(command, stderr=subprocess.PIPE)
     status = proc.wait()
     error_string = proc.stderr.read()
@@ -41,24 +46,38 @@ def tesseract(input_filename, output_filename):
 
 
 def read(output_filename):
-    f = open(output_filename, 'rb')
+    """
+    No longer used
+    """
+    filestream = open(output_filename, 'rb')
     try:
-        print(f.read().decode('utf-8').strip())
+        print(filestream.read().decode('utf-8').strip())
     finally:
-        f.close()
+        filestream.close()
 
 
 def tts(input_filename1="out1.txt", input_filename2="out2.txt", output_filename="out.mp3"):
-    str = "卖 "
-    f = open(input_filename1)
-    str += f.readline()
+    """
+    Convert text to sound
+    """
+    string = "卖 "
+    filestream = open(input_filename1)
+    try:
+        tmp = int(filestream.readline())
+    except ValueError:
+        return 0
+    string += str(tmp)
     # print(line, end='')
-    f.close()
-    f = open(input_filename2)
-    str += "买 " + f.readline()
-    f.close()
-    tts = gTTS(text=str, lang='zh-cn', slow=False)
-    tts.save(output_filename)
+    filestream.close()
+    filestream = open(input_filename2)
+    try:
+        tmp = int(filestream.readline())
+    except ValueError:
+        return 0
+    string += "买 " + str(tmp)
+    filestream.close()
+    tts_ = gTTS(text=string, lang='zh-cn', slow=False)
+    tts_.save(output_filename)
 
 
 main()
